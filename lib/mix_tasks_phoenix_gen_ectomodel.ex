ed5a639ec0928eacba36a85e37e42966b51a5955
greggreg <greggreg@itsgreggreg.com>
2015-01-21 15:11:38 -0800

    added schema to model generator

--- lib/mix_tasks_phoenix_gen_ectomodel.ex
+++ lib/mix_tasks_phoenix_gen_ectomodel.ex
@@ -6,13 +6,22 @@ defmodule Mix.Tasks.Phoenix.Gen.Ectomodel do
   @shortdoc "Generate an Ecto Model for a Phoenix Application"
 
   def run(opts) do
-    {_switches, [model_name | _actions], _files} = OptionParser.parse opts
+    {_switches, [model_name | fields], _files} = OptionParser.parse opts
     model_name_camel = camelize model_name
     app_name_camel = camelize Atom.to_string(Mix.Project.config()[:app])
+    fields = for field <- fields do
+      case String.split(field, ":") do
+        [name]             -> [name, ""]
+        [name, "datetime"] -> [name, "datetime, default: Ecto.DateTime.utc"]
+        [name, type]       -> [name, type]
+      end
+    end
 
     bindings = [
       app_name: app_name_camel,
-      model_name: model_name_camel
+      model_name_camel: model_name_camel,
+      model_name_under: model_name,
+      fields: fields
     ]
 
     # generate the model file
