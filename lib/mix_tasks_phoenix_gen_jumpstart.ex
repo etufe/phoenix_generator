8f4ea0bda067401e06a22530e16f193d09508d35
greggreg <greggreg@itsgreggreg.com>
2015-01-23 17:34:48 -0800

    check for ecto in jumpstart

--- lib/mix_tasks_phoenix_gen_jumpstart.ex
+++ lib/mix_tasks_phoenix_gen_jumpstart.ex
@@ -1,6 +1,7 @@
 defmodule Mix.Tasks.Phoenix.Gen.Jumpstart do
   use Mix.Task
   import Phoenix.Gen.Utils
+  import Mix.Shell.IO, only: [info: 1, error: 1]
 
   @shortdoc "Generate a bunch of setup stuff"
   @moduledoc """
@@ -23,24 +24,30 @@ defmodule Mix.Tasks.Phoenix.Gen.Jumpstart do
       other -> ["--repo", other]
     end
 
-    Mix.Tasks.Ecto.Gen.Repo.run(repo)
+    if Mix.Task.task? Mix.Tasks.Ecto.Gen.Repo do
+      Mix.Tasks.Ecto.Gen.Repo.run(repo)
 
-    # Add the repo worker to supervision tree
-    p_path = Path.join ["lib", app_name<>".ex"]
-    [_, p_top, p_bot] = Regex.run ~r/(.*children = \[\n)(.*)/s, File.read!(p_path)
-    File.write! p_path, p_top<>"      worker(#{List.last repo}, [])\n"<>p_bot
-    IO.puts "The worker has been added for you!"
+      # Add the repo worker to supervision tree
+      p_path = Path.join ["lib", app_name<>".ex"]
+      [_, p_top, p_bot] = Regex.run ~r/(.*children = \[\n)(.*)/s, File.read!(p_path)
+      File.write! p_path, p_top<>"      worker(#{List.last repo}, [])\n"<>p_bot
+      info "The worker has been added for you!"
 
-    # override postgres settings if --postgres-url
-    if postgres_url = Keyword.get(switches, :postgres_url) do
-      c_path    = Path.join ~w|config config.exs|
-      re        = Regex.compile! "(?:[a-zA-Z\":_ ,]*\n+){4}$" #last 4 lines
-      [top | _] = Regex.split re, File.read!(c_path)
-      File.write! c_path, top<>"  url: \""<>postgres_url<>"\"\n"
-      IO.puts "Postgres settings modified. Check config/config.exs."
+      # override postgres settings if --postgres-url
+      if postgres_url = Keyword.get(switches, :postgres_url) do
+        c_path    = Path.join ~w|config config.exs|
+        re        = Regex.compile! "(?:[a-zA-Z\":_ ,]*\n+){4}$" #last 4 lines
+        [top | _] = Regex.split re, File.read!(c_path)
+        File.write! c_path, top<>"  url: \""<>postgres_url<>"\"\n"
+        info "Postgres settings modified. Check config/config.exs."
+      end
+
+      info "To create a db run: mix ecto.create"
+    else
+      error "Please include ecto in your project dependencies."
+      error "https://github.com/elixir-lang/ecto"
     end
 
-    IO.puts "To create a db run: mix ecto.create"
 
   end
 
